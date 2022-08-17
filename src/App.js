5802bc847cc2f390db2464c4921feb6a4f670674
hellskater <ksrinivasrao531@gmail.com>
2022-08-17 12:30:25 +0530

    created a basic working model

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
 import { useRef } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { useGLTF, PresentationControls, Environment, ContactShadows, Html } from '@react-three/drei'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import { Environment, ContactShadows } from '@react-three/drei'
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
 
 export default function App() {
   return (
     <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
-      <ambientLight intensity={0.5} />
+      <ambientLight intensity={0.1} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} shadow-mapSize={[512, 512]} castShadow />
-      <PresentationControls
-        global
-        config={{ mass: 2, tension: 500 }}
-        snap={{ mass: 4, tension: 1500 }}
-        rotation={[0, 0.3, 0]}
-        polar={[-Math.PI / 3, Math.PI / 3]}
-        azimuth={[-Math.PI / 1.4, Math.PI / 2]}>
-        <Watch rotation={[-Math.PI / 2, 0, 0]} position={[0, 0.25, 0]} scale={0.003} />
-      </PresentationControls>
+      <Card />
       <ContactShadows position={[0, -1.4, 0]} opacity={0.75} scale={10} blur={2.5} far={4} />
       <Environment preset="city" />
     </Canvas>
   )
 }
 
-function Watch(props) {
+function Card(props) {
   const ref = useRef()
-  const { nodes, materials } = useGLTF('/watch-v1.glb')
+  const colorMap = useLoader(TextureLoader, '/nft1.jpg')
+
   useFrame((state) => {
     const t = state.clock.getElapsedTime()
     ref.current.rotation.x = -Math.PI / 1.75 + Math.cos(t / 4) / 8
@@ -34,14 +28,10 @@ function Watch(props) {
   })
   return (
     <group ref={ref} {...props} dispose={null}>
-      <mesh geometry={nodes.Object005_glass_0.geometry} material={materials.glass}>
-        <Html scale={100} rotation={[Math.PI / 2, 0, 0]} position={[180, -350, 50]} transform occlude>
-          <div className="annotation">
-            6.550 $ <span style={{ fontSize: '1.5em' }}>🥲</span>
-          </div>
-        </Html>
+      <mesh castShadow position={[0, 0.5, 0]}>
+        <boxGeometry args={[2, 0.1, 2]} />
+        <meshStandardMaterial color="lightgray" map={colorMap} />
       </mesh>
-      <mesh castShadow receiveShadow geometry={nodes.Object006_watch_0.geometry} material={materials.watch} />
     </group>
   )
 }
